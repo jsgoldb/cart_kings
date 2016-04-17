53068d65cd2e41ffd0320e922e87ae22223d8858
jsgoldb <jgoldb@yahoo.com>
2016-04-17 13:10:30 -0500

    bug fixes

--- app/validators/phone_validator.rb
+++ app/validators/phone_validator.rb
@@ -2,7 +2,7 @@ class PhoneValidator < ActiveModel::EachValidator
 
   def validate_each(user, phone, value)
     value.gsub!(/[\s-]*/, "")
-    unless value =~ /^((\+\d{3,4})?\d{7,8})?$/
+    unless value =~ /\D*([2-9]\d{2})(\D*)([2-9]\d{2})(\D*)(\d{4})\D*/
       user.errors[phone] << (options[:message] || "is not a valid number")
     end
   end
